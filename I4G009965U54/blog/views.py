6383388031dbde0de1458f1549fd930a4f906cc7
StevenVolk <36421837+StevenVolk@users.noreply.github.com>
2022-06-28 09:13:13 -0400

    new change

--- I4G009965U54/blog/views.py
+++ I4G009965U54/blog/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
-from .models import Blog
+from .models import Post
 
 class PostListView(ListView):
     model = Post
 
 class PostCreateView(CreateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
 class PostDetailView(DetailView):
     model = Post
 
 class PostUpdateView(UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
 
 class PostDeleteView(UpdateView):
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
